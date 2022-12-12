0baf1fae0d510c2a8fe6bc98cb8ceafef780937d
Jon Ackley <jonackley16@gmail.com>
2022-12-12 14:35:39 -0500

    fixing links in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const NavBar = ({ setLogin, username, token }) => {
 
     const handleLogout = () => {
-        axios.post(HTTPS/LOGOUTENDPT,
+        axios.post('HTTPS/LOGOUTENDPT',
         { headers: { Authorization: `Token ${token}`,}, })
         .then(() => setLogin('', null))
         .catch(() => setLogin('', null))
@@ -14,16 +14,18 @@ export const NavBar = ({ setLogin, username, token }) => {
         <header className='header'>
             <h1 className='head'>Focus, Agent.</h1>
                 <nav className='navigation'>
-                    <button className='button'><Link to="/login" onClick={() => setLogin(null)}></Link>Login</button>
-                    <button className='button'><Link to="/"></Link>Home</button>
-                    <button className='button'><Link to="/profile"></Link>Your Profile</button>
-                    <button className='button'><Link to="/wins"></Link>Wins!</button>
-                    <button className='button'><Link to="/starrs"></Link>STARR stories</button>
-                    <button className='button'><Link to="/targetcompanies"><🎯 Companies/Link></button>
-                    <button className='button'><Link to="/targetjobs"></Link>🎯 Jobs</button>
-                    <button className='button'><Link to="/resumes"></Link>Resume</button>
-                    <button className='button'><Link to="/coverletters"></Link>Cover Letters</button>
-                    <button className='button'><Link to="/questions"></Link>Questions</button>
+                    <div>
+                        <Link className='button is-success is-small' to="/login" onClick={() => setLogin(null)}>Login</Link>
+                        <Link className='button is-success is-small' to="/">Home</Link>
+                        <Link className='button is-success is-small' to="/Profile">Your Profile</Link>
+                        <Link className='button is-success is-small'to="/Wins">Wins!</Link>
+                        <Link className='button is-success is-small'to="/Starrs">STARR stories</Link>
+                        <Link className='button is-success is-small'to="/">Home</Link>
+                        <Link className='button is-success is-small'to="/TargetCompanies">🎯 Companies</Link>
+                        <Link className='button is-success is-small'to="/TargetJobs">🎯 Jobs</Link>
+                        <Link className='button is-success is-small'to="/Resumes">Resume</Link>
+                        <Link className='button is-success is-small'to="/CoverLetters">Cover Letters</Link>
+                    </div>
                 </nav>
         </header>
     )
