a8c044b9b79b4fb7bce6a86f19752043c0dd8bf5
Jon Ackley <jonackley16@gmail.com>
2022-12-31 13:09:46 -0500

    Dossier dnd work

---
+++ src/components/jobcompenents/TJDossier.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import { requestTargetJobDetail } from '../requests/JobRequests';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components'
+
+export const TJDossier = () => {
+
+
+    return (
+        dossier container
+    )
+}
---
+++ src/components/jobcompenents/TJDossierView.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+import { requestTargetJobDetail } from '../requests/JobRequests';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components'
