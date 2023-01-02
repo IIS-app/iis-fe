005a064feff492b5ee6604a8f0df7e40e844ffe1
Webster Grimes <cwebsterg@mac.com>
2023-01-02 12:29:47 -0500

    trying to render data, it is rerendering after get

--- src/components/usercomponents/Profile.js
+++ src/components/usercomponents/Profile.js
@@ -1,49 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { requestUserInfo } from '../requests/GeneralRequests';
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import 'react-quill/dist/quill.bubble.css'
+
 
 export const Profile = ({ token }) => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [userId, setUserId] = useState('');
+    const [userFirstName, setUserFirstName] = useState('');
+    const [userLastName, setUserLastName] = useState('');
     const [codename, setCodename] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [personalPitch, setPersonalPitch] = useState([]);
+    const [userEmail, setUserEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    const [personalNotes, setPersonalNotes] = useState('');
     const [linkedin, setLinkedin] = useState('');
     const [ github, setGithub] = useState('');
+    const [ codePen, setCodePen] = useState('');
+    const [ portfolio, setPortfolio] = useState('');
+    const [error, setError] = useState(null)
 
 
     useEffect(() => {
-    requestUserInfo(token).then(response => {
-        const { first_name, last_name, codename, email, password, personal_pitch, linkedin, github } = response.data;
-        setFirstName(first_name);
-        setLastName(last_name);
-        setCodename(codename);
-        setEmail(email);
-        setPassword(password);
-        setPersonalPitch(personal_pitch);
-        setLinkedin(linkedin);
-        setGithub(github);
-        });
-    }, [token]);
+        setError(null)
+        requestUserInfo(token)
+            .then(res => {
+                setUserId(res.id)
+                console.log(userFirstName)
+                setUserFirstName(res.first_name);
+                console.log(userFirstName)
+                setUserLastName(res.last_name);
+                setCodename(res.codename);
+                setUserEmail(res.email);
+                // setPassword(res.password);
+                setPersonalNotes(res.personal_pitch);
+                setLinkedin(res.linkedin);
+                setGithub(res.github);
+                setCodePen(res.codepen);
+                setPortfolio(res.portfolio);
+                console.log(res)
+            })
+            .catch(error => setError(error.message))
+            },[token])
+
+        const modules = {
+            toolbar: [
+                [{ 'header': [1, 2, 3, false] }],
+                [{ 'bold': true }, { 'italic': true }, { 'underline': true }, { 'strike': true }],
+                [{ list:  "ordered" }, { list:  "bullet" }],
+                ["blockquote", "code-block"],
+            ]
+        }
+    
+        
 
     return (
         <div className='container-profile'>
+        {error && <div className="error">{ error }</div>}
+
             <h2>The facts.</h2>
-            <div>
-                <p>First Name: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Codename: {codename}</p>
-                <p>Email: {email}</p>
-                <p>Password: {password}</p>
-                <p>Personal Pitches:</p>
-            <ul>
-                {personalPitch.map(pitch => (
-                <li key={pitch}>{pitch}</li>
-                ))}
-            </ul>
-                <p>LinkedIn: {linkedin}</p>
-                <p>Github: {github}</p>          
+            <div key={userId}>
+                {console.log(codename)}
+                <p>{`First Name: ${userFirstName}`}</p>
+                <p>{`Last Name: ${userLastName}`}</p>
+                <p>{`Codename: ${codename}`}</p>
+                <p>{`Email: ${userEmail}`}</p>
+                {/* <p>{`Password: ${password}`}</p> */}
+                <p>{`LinkedIn: ${linkedin}`}</p>
+                <p>{`Github: ${github}`}</p>          
+                <p>Personal Notes:</p>
+                <ReactQuill
+                    modules={modules}
+                    theme="snow"
+                    className='custom-quill'
+                    id='personalNotes'
+                    style={{height: 'auto', minHeight: '200px'}}
+                    name='personalNotes'
+                    maxLength={2000}
+                    onChange={(value) => setPersonalNotes(value)}
+                    
+                />
+
             </div>
             
         </div>
